e1327f3451b1bab5fba987602ec7e7647f697811
Inflearn <haru2175@naver.com>
2024-12-05 15:47:47 +0900

    네이버 쇼핑 API 검색

--- 4team-react/src/App.js
+++ 4team-react/src/App.js
@@ -1,16 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SearchPage from './components/SearchPage';
 
 function App() {
     const [data, setData] = useState("");
 
     useEffect(() => {
-        axios.get("http://localhost:8989/api/data")
+        axios
+            .get("http://localhost:8989/api/data")
             .then((response) => setData(response.data))
             .catch((error) => console.error("Error fetching data:", error));
     }, []);
 
-    return <div>{data}</div>;
+    return (
+        <div>
+            <h1>Spring Boot와 React 연동</h1>
+            <div>{data}</div>
+
+            {/* SearchPage 컴포넌트 추가했습니다. */}
+            <SearchPage/>
+        </div>
+    );
 }
 
 export default App;
