599b4404c3d84e41ad0f8eb269143f7a0042143b
Inflearn <haru2175@naver.com>
2024-12-06 15:47:57 +0900

    추천 메뉴 구현

--- 4team-react/src/App.js
+++ 4team-react/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import SearchPage from './components/SearchPage';
+import MenuList from "./components/MenuList";
 
 function App() {
     const [data, setData] = useState("");
@@ -14,11 +15,13 @@ function App() {
 
     return (
         <div>
-            <h1>Spring Boot와 React 연동</h1>
+            <h1>네이버 쇼핑 API 검색</h1>
             <div>{data}</div>
 
             {/* SearchPage 컴포넌트 추가했습니다. */}
             <SearchPage/>
+            <h1>오늘의 메뉴 추천 시스템</h1>
+            <MenuList/>
         </div>
     );
 }
