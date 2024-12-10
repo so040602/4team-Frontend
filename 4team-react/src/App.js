d5fa9843740fcd886750907bea2a86d98b1bfa7a
leejongchan89 <cleejy89@icloud.com>
2024-12-10 12:33:54 +0900

    App.js 파일 수정

--- 4team-react/src/App.js
+++ 4team-react/src/App.js
@@ -4,26 +4,11 @@ import AppRouter from "./AppRouter";
 import "./App.css";
 
 function App() {
-    const [data, setData] = useState("");
-
-    useEffect(() => {
-        axios
-            .get("http://localhost:8989/api/data")
-            .then((response) => setData(response.data))
-            .catch((error) => console.error("Error fetching data:", error));
-    }, []);
-
-    return (
-        <div>
-            <h1>네이버 쇼핑 API 검색</h1>
-            <div>{data}</div>
-
-            {/* SearchPage 컴포넌트 추가했습니다. */}
-            <SearchPage/>
-            <h1>오늘의 메뉴 추천 시스템</h1>
-            <MenuList/>
-        </div>
-    );
+  return (
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  );
 }
 
 export default App;
