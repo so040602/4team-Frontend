df7b3141430d369855331238a0b0634abb08b325
Inflearn <haru2175@naver.com>
2024-12-11 18:06:45 +0900

    gg

--- 4team-react/src/AppRouter.js
+++ 4team-react/src/AppRouter.js
@@ -4,18 +4,31 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import OAuth2Callback from "./pages/OAuth2Callback";
 import Navbar from "./components/Navbar";
-
+import MainPage from "./pages/MainPage";
+import ReviewList from "./pages/ReviewList";
+import ReviewForm from "./pages/ReviewForm";
+import ReviewDetail from "./pages/ReviewDetail";
 
 function AppRouter() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        {/* <Route path="/" element={<MainPage />} /> 메인 페이지용 */}
-        <Route path="/Menu" element={<Menu />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/oauth2/callback" element={<OAuth2Callback />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/*" element={
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/Menu" element={<Menu />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/oauth2/callback" element={<OAuth2Callback />} />
+              <Route path="/reviews" element={<ReviewList />} />
+              <Route path="/reviews/new" element={<ReviewForm />} />
+              <Route path="/reviews/:id" element={<ReviewDetail />} />
+              <Route path="/reviews/:id/edit" element={<ReviewForm />} />
+            </Routes>
+          </>
+        } />
       </Routes>
     </BrowserRouter>
   );
