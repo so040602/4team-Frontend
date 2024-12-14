3b15292b6ee4a8f23dac811c9485fad37189e188
haru <haru2175@naver.com>
2024-12-14 15:26:49 +0900

    gg

--- 4team-react/src/App.js
+++ 4team-react/src/App.js
@@ -1,29 +1,14 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import SearchPage from './components/SearchPage';
-import MenuList from "./components/MenuList";
+import React from "react";
+import { AuthProvider } from "./contexts/AuthContext";
+import AppRouter from "./AppRouter";
+import "./App.css";
 
 function App() {
-    const [data, setData] = useState("");
-
-    useEffect(() => {
-        axios
-            .get("http://localhost:8989/api/data")
-            .then((response) => setData(response.data))
-            .catch((error) => console.error("Error fetching data:", error));
-    }, []);
-
-    return (
-        <div>
-            <h1>네이버 쇼핑 API 검색</h1>
-            <div>{data}</div>
-
-            {/* SearchPage 컴포넌트 추가했습니다. */}
-            <SearchPage/>
-            <h1>오늘의 메뉴 추천 시스템</h1>
-            <MenuList/>
-        </div>
-    );
+  return (
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  );
 }
 
 export default App;
