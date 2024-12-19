9fec48fc33243ae393b18b34fac7fa26b27ac00a
leejongchan89 <cleejy89@icloud.com>
2024-12-19 10:14:11 +0900

    레시피입력폼 구현, sweatalert 라이브러리 추가

--- 4team-react/src/AppRouter.js
+++ 4team-react/src/AppRouter.js
@@ -8,34 +8,40 @@ import MainPage from "./pages/MainPage";
 import ReviewList from "./pages/ReviewList";
 import ReviewForm from "./pages/ReviewForm";
 import ReviewDetail from "./pages/ReviewDetail";
-import MyPage from './pages/MyPage';
+import MyPage from "./pages/MyPage";
 import ChatBot from "./components/chatbot/ChatBot";
-import UserProfile from './pages/UserProfile';
+import UserProfile from "./pages/UserProfile";
 import RefriUI from "./pages/MyRefriUI";
+import RecipeCreate from "./pages/RecipeCreate";
+
 function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/*" element={
-          <>
-            <Navbar />
-            <Routes>
-              <Route path="/Menu" element={<Menu />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/oauth2/callback" element={<OAuth2Callback />} />
-              <Route path="/reviews" element={<ReviewList />} />
-              <Route path="/reviews/new" element={<ReviewForm />} />
-              <Route path="/reviews/:id" element={<ReviewDetail />} />
-              <Route path="/reviews/:id/edit" element={<ReviewForm />} />
-              <Route path="/mypage" element={<MyPage />} />
-              <Route path="/chatbot/Chatbot" element={<ChatBot />} />
-              <Route path="/users/:memberId" element={<UserProfile />} />
-              <Route path="/refriUI" element={<RefriUI/>}/>
-            </Routes>
-          </>
-        } />
+        <Route
+          path="/*"
+          element={
+            <>
+              <Navbar />
+              <Routes>
+                <Route path="/Menu" element={<Menu />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/oauth2/callback" element={<OAuth2Callback />} />
+                <Route path="/reviews" element={<ReviewList />} />
+                <Route path="/reviews/new" element={<ReviewForm />} />
+                <Route path="/reviews/:id" element={<ReviewDetail />} />
+                <Route path="/reviews/:id/edit" element={<ReviewForm />} />
+                <Route path="/mypage" element={<MyPage />} />
+                <Route path="/chatbot/Chatbot" element={<ChatBot />} />
+                <Route path="/users/:memberId" element={<UserProfile />} />
+                <Route path="/refriUI" element={<RefriUI />} />
+                <Route path="/recipe/create" element={<RecipeCreate />} />
+              </Routes>
+            </>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
